933ea45740e9495c0b8af5c41dc2dfaba56aaa11
gamavgaming-beep <gamavgaming@gmail.com>
2026-07-20 20:40:39 +0530

    Add CubeRotation class for Rubik's Cube animation

--- js/cube-rotation.js
+++ js/cube-rotation.js
@@ -1,1 +1,21 @@
 import * as THREE from "three";
+
+export class CubeRotation {
+
+    constructor(rubiksCube) {
+
+        this.cube = rubiksCube;
+
+        this.animating = false;
+
+        this.targetQuaternion = new THREE.Quaternion();
+
+        this.targetQuaternion.copy(this.cube.quaternion);
+
+        this.rotationAngle = Math.PI / 2;
+
+        this.rotationSpeed = 0.12;
+
+    }
+
+}
